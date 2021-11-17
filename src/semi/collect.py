737cdae314c22ac25defc4bc2d581de464a5739f
uabcoimbra <choiwah.chow@gmail.com>
2021-11-17 18:48:07 -0500

    added collect.py to post-process results

--- src/semi/collect.py
+++ src/semi/collect.py
@@ -16,39 +16,43 @@ import sys
 def extract_data(file_path):
     order = -1
     error = ""
+    file_base_name = os.path.basename(file_path)
+    names = file_base_name.split("_")
+    line_no = int(names[0])
+    subvariety = (int(names[1]), int(names[2]))
+    variety = (int(names[4]), int(names[5].split(".")[0]))
     with (open(file_path)) as fp:
-        line = fp.readline()
-        if line.startswith("interpretation("):
-            pos = line.find(",")
-            order = int(line[16:pos])
-        elif line.startswith("Current CPU time: " ):
-            pos1 = line.find("(total CPU time: ")
-            pos2 = line.rfind(" seconds")
-            cpu_time = int(line[pos1, pos2])
-        elif line.startswith("For domain size "):
-            domain_size = int(line[16:-1])
-        elif line.startswith("Exiting with failure."):
-            error = "Exiting with failure"
-        elif "(max_megs_no)" in line:
-            error = "max_megs_no"
-        elif "(max_sec_no)" in line:
-            error = "max_sec_no"                  
+        for line in fp.readlines():
+            if line.startswith("interpretation("):
+                pos = line.find(",")
+                order = int(line[16:pos])
+            elif line.startswith("Current CPU time: " ):
+                pos1 = line.find("(total CPU time: ")
+                pos2 = line.rfind(" seconds")
+                cpu_time = float(line[pos1+16:pos2])
+            elif line.startswith("For domain size "):
+                domain_size = int(line[16:-2])
+            elif line.startswith("Exiting with failure."):
+                error = "Exiting with failure"
+            elif line.startswith("Process ") and "(max_megs_no)" in line:
+                error = "max_megs_no"
+            elif line.startswith("Process ") and "(max_sec_no)" in line:
+                error = "max_sec_no"                  
 
     if error == "":
-        return (order, cpu_time, error)
+        return (line_no, subvariety, variety, order, cpu_time, error)
     else:
-        return (domain_size, cpu_time, error)
+        return (line_no, subvariety, variety, domain_size, cpu_time, error)
 
+
+def extract_all_data(out_dir):
+    for file in os.listdir(out_dir):
+        results = extract_data(os.path.join(out_dir, file))
+        print(results)
 
 if __name__ == "__main__":
-    excel_file = sys.argv[1]
-    n1 = int(sys.argv[2])
-    n2 = int(sys.argv[3])
-    if n1 < 1 or n1 > n2:
-        print("<from excel row> must not be greater than <to excel row>.")
+    if len(sys.argv) > 1:
+        out_dir = sys.argv[1]
     else:
-        if len(sys.argv) > 4:
-            out_dir = sys.argv[4]
-        else:
-            out_dir = "."
-        v = gen_mace4_files(excel_file, range(n1, n2+1), out_dir)
+        out_dir = "."
+    v = extract_all_data(out_dir)
